023e2cb92070f8734ffa49f3929bd3e590886ca3
Mustapha Conteh <mco176@uclive.ac.nz>
2023-11-09 01:22:14 +1300

    Implemented email verification functionality

--- index.ts
+++ index.ts
@@ -5,6 +5,9 @@ import * as bcrpyt from 'bcrypt';
 import * as Joi from 'joi';
 import * as jsonwebtoken from 'jsonwebtoken';
 import * as nodemailer from 'nodemailer';
+import * as google from 'googleapis';
+const OAuth2 = google.Auth.OAuth2Client;
+
 const cors = require('cors');
 const app = express()
 const port = 3000
@@ -15,28 +18,39 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-const sendEmail = async (email : string, subject : string, text : string) => {
-  try {
-    const transporter = nodemailer.createTransport({
-    service: process.env["email_service"],
-      auth: {
-        user: process.env["email_address"],
-        pass: process.env["email_password"],
-      },
-    });
 
-    await transporter.sendMail({
-      from: process.env.USER,
-      to: email,
-      subject: subject,
-      text: text,
-    });
-    console.log("email sent sucessfully");
-  } catch (error) {
-    console.log("email not sent");
-    console.log(error);
-  }
-};
+const sendEmail = async (email : string, subject : string, text : string) => {
+    try {
+       const oauth2Client = new OAuth2(
+           process.env.client_id,
+           process.env.client_secret,
+           "https://developers.google.com/oauthplayground"
+         );
+         oauth2Client.setCredentials({
+           refresh_token: process.env.refresh_token,
+         });
+         const transporter = nodemailer.createTransport({
+           service: "gmail",
+           auth: {
+             type: "OAuth2",
+             user: process.env.email_address,
+             clientId: process.env.client_id,
+             clientSecret: process.env.client_secret,
+             refreshToken: process.env.refresh_token,
+           },
+         });
+      await transporter.sendMail({
+        from: process.env.email_address,
+        to: email,
+        subject: subject,
+        text: text,
+      });
+      console.log("email sent sucessfully");
+    } catch (error) {
+      console.log("email not sent");
+      console.log(error);
+    }
+  };
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
 })
@@ -107,10 +121,10 @@ app.get('/api/users', authorization, async (req: Request, res: Response) => {
     console.log("req.body.userId: ", req.body.userId)
 });
 
-app.get('/verify/:token', async (req: Request, res: Response) => {
+app.get('/api/verify/:token', async (req: Request, res: Response) => {
     const token = req.params.token;
     try {
-    
+        verifyEmail(token) 
         res.sendStatus(200);
     } catch (err) {
         res.sendStatus(401);
@@ -123,8 +137,7 @@ const verifyEmail = async (token: string) => {
         if (typeof decoded === 'string') {
             throw new Error('Invalid token');
         }
-        const email = decoded.userId;
-        const result: pg.QueryResult = await db.query(`UPDATE social_media.users SET email_verified = true WHERE email = '${email}'`);
+        const result: pg.QueryResult = await db.query(`UPDATE social_media.users SET active = true WHERE user_id = '${decoded.userId}'`);
         console.log("result: ", result);
     } catch (err) {
         console.log("err: ", err);
@@ -169,9 +182,8 @@ const registerUser = async (response : Response, email: string, password: string
         const salt = await bcrpyt.genSalt(10);
         const hashedPassword = await bcrpyt.hash(password, salt);
         const addUserResult = await addNewUser(email, hashedPassword, salt);
-        const emailToken = await bcrpyt.genSalt();
+        const emailToken = await jsonwebtoken.sign({userId : addUserResult.rows[0].user_id}, process.env["jwt_secret"] as string);
         const verificationMessage = `${process.env.BASE_URL}/api/verify/${emailToken}`
-        await addEmailVerificationToken(addUserResult.rows[0].user_id, emailToken)
         sendEmail(email, "Verify your Email", verificationMessage)
         console.log("User added");
         response.sendStatus(201);
