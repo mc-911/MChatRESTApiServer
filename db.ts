d60ceaaa17383b488bbdaaadb496242605161bee
Mustapha Conteh <mco176@uclive.ac.nz>
2023-11-29 01:24:13 +1300

    Updated how pool is created

--- db.ts
+++ db.ts
@@ -1,25 +1,17 @@
-const { Pool, Client } = require('pg');
+import { Pool } from 'pg'
 
 require('dotenv').config()
-if (process.env.DATABASE_URL) {
-  const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
-  module.exports = {
-    query: (text: string, params?: any[]) => client.query(text, params)
-  };
-} else {
-  const pool = new Pool({
-    user: process.env["db_username"],
-    password: process.env["db_password"],
-    host: process.env["db_host"],
-    port: process.env["db_port"],
-    database: process.env["database_name"]
-  });
-  module.exports = {
-    query: (text: string, params?: any[]) => pool.query(text, params)
-  };
-}
+const pool = process.env.DATABASE_URL ? new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+}) : new Pool({
+  user: process.env["db_username"],
+  password: process.env["db_password"],
+  host: process.env["db_host"],
+  port: parseInt(process.env["db_port"]!),
+  database: process.env["database_name"]
+})
+
+module.exports = {
+  query: (text: string, params?: any[]) => pool.query(text, params)
+};
