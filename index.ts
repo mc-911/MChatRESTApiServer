6eb025741108aac43cb5863aa419eb461c5e9c4e
Mustapha Conteh <mco176@uclive.ac.nz>
2023-11-29 21:33:01 +1300

    Added url redirect to profile picture on s3

--- index.ts
+++ index.ts
@@ -10,21 +10,15 @@ import FormData from 'form-data';
 import { unlink } from 'fs';
 import validator from 'validator'
 import { error } from 'console';
-import { S3 } from '@aws-sdk/client-s3';
+import { S3, GetObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT ? process.env.PORT : 3000
 const db = require('./db');
 const cookieParser = require('cookie-parser')
 
-// const storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         cb(null, 'uploads/');
-//     },
-//     filename: (req, file, cb) => {
-//         cb(null, Date.now() + '-' + file.originalname);
-//     }
-// });
 const s3 = new S3({
     credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
@@ -45,8 +39,6 @@ const upload = multer({
 
     }
 });
-
-const updateProfileImage = upload.single("profilePicture");
 
 app.use(
     cors({
@@ -271,7 +263,13 @@ app.get('/api/users/:userId/profilePicture', authorization, validateUserIdParam,
     if (!result.rowCount) {
         res.sendStatus(404);
     } else if (result.rows[0].profile_picture) {
-        res.sendFile(`uploads/${result.rows[0].profile_picture}`, { root: __dirname });
+        const command = new GetObjectCommand({
+            Bucket: process.env.AWS_S3_BUCKET_NAME as string,
+            Key: result.rows[0].profile_picture,
+        })
+        const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
+        res.redirect(url);
+
     } else {
         res.sendStatus(404)
     }
